28934dfdf5a22803c947081068cd566613078f5d
Ian Dart <ian.dart@idc.technology>
2019-11-30 16:55:02 +0000

    Start incorporating Weather API

--- index.js
+++ index.js
@@ -14,42 +14,56 @@ const {
 
 const app = express();
 
+const host = 'api.openweathermap.org';  OpenWeather API link
+const weatherAPI = process.env.API;
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Weather API interaction
 
+function callWeatherApi (city, date) {
+  return new Promise ((resolve, reject) {
+    //Create the path for the HTTP request 
+    let path = host + '/data/2.5/weather?q=' + city + '&appid=' + weatherAPI;
+    console.log('Path: ' + path);
+
+  })
+}
+
 // DialogFlow Fulfillment
 
-function gedate(agent) {
+function getweather(agent) {
   agent.add(
     "The General Election will take place on Thursday 12th December 2019"
   );
-  agent.add(
-    `The next General Election is planned to take place on the 12th December 2019`
-  );
-}
-
-function welcome(agent) {
-  agent.add(`???`);
-}
-
-function broadband(agent) {
-  agent.add(`???`);
 }
 
 function WebhookProcessing(req, res) {
   const agent = new WebhookClient({ request: req, response: res });
 
   let intentMap = new Map();
-  intentMap.set("When is the General Election", gedate);
+  intentMap.set("Get Weather", getweather);
   intentMap.set("Default Welcome Intent", welcome);
   intentMap.set("What Gigabit Broadband Upgrade", broadband);
   agent.handleRequest(intentMap);
 }
 
+// DialogFlow Entry Point
+
 app.post("/weather", (req, res) => {
   console.log("DialogFlow Server Reached!!!");
+
+  // Determine what City is being requested
+  let city = req.body.queryResult.parameters["geo-city"] || "Not provided";
+  console.log(`City being requested is ${city}`);
+
+  // Determine what date is being requested
+  date = "";
+  if (req.body.queryResult.parameters["date"]) {
+    let date = req.body.queryResult.parameters["date"];
+    console.log("Date: " + date);
+  }
 
   WebhookProcessing(req, res);
 });
